1a4d13391df142b5fa35b017feb221b3d38913e4
Ayodele Oniosun <ayodele@ogunlabs.com>
2022-06-23 10:36:15 +0100

    chore: update model relationships and return types

--- app/Models/Ads.php
+++ app/Models/Ads.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ads extends Model
@@ -12,22 +14,22 @@ class Ads extends Model
 
     protected $guarded = ['id'];
 
-    public function sortOptions()
+    public function sortOptions(): HasMany
     {
         return $this->hasMany(AdsSortOption::class);
     }
 
-    public function categories()
+    public function categories(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
 
-    public function subCategories()
+    public function subCategories(): BelongsTo
     {
         return $this->belongsTo(SubCategory::class);
     }
 
-    public function seller()
+    public function seller(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
