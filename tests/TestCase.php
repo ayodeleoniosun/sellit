7790d04d97a578079c3a33a78dbe800f9492682d
Ayodele Oniosun <heywire4u@gmail.com>
2022-07-05 23:43:43 +0100

    chore: setup pest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,15 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected string $baseUrl = 'http://sellit.test';
+
+    protected string $apiBaseUrl;
+
+    protected function setup(): void
+    {
+        parent::setUp();
+        $this->apiBaseUrl = $this->baseUrl . '/api/v1';
+        $this->faker = \Faker\Factory::create();
+    }
 }
